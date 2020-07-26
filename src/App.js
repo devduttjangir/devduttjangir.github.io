f76a7949052fd3825906345c37f244c6395368f1
Devdutt Jangir <devdutt.jangir@3pillarglobal.com>
2020-07-26 12:32:18 +0530

    remove test file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import "./App.css";
-
 import React from "react";
-import logo from "./logo.svg";
 
 function App() {
-  return <div className="App">Surge Integration</div>;
+  return <div>Surge Integration</div>;
 }
 
 export default App;
