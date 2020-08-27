271981cf3cf1d503e0f833540b4339dd0b4aeae4
devdutt jangir <devdutt.jangir@3pillarglobal.com>
2020-08-27 15:21:40 +0530

    build uploaded

---
+++ precache-manifest.bd6152d5e2c7472a95e8fa6d440f1d53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d1dac4e04c09ce0143761f3a732a81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ce5bb0d7056459e4c6e",
+    "url": "/static/css/main.90eb1194.chunk.css"
+  },
+  {
+    "revision": "dbbb09f1ec10c3d6b654",
+    "url": "/static/js/2.5a3e3268.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5a3e3268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce5bb0d7056459e4c6e",
+    "url": "/static/js/main.11a5be79.chunk.js"
+  },
+  {
+    "revision": "3cc9d6562f64fc197f20",
+    "url": "/static/js/runtime-main.fc30dc24.js"
+  }
+]);
--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-function App() {
-  return <div>Surge Integration with help of travis CI</div>;
-}
-
-export default App;
